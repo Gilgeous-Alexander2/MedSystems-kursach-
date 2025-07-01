6c7b2f6779a7ed82f22c948a18df7452f11ca3fc
Gilgeous-Alexander2 <117025505+Gilgeous-Alexander2@users.noreply.github.com>
2025-07-01 10:18:29 +0300

    Update main-page.js

--- env/my-app/src/main-page/main-page.js
+++ env/my-app/src/main-page/main-page.js
@@ -3,7 +3,7 @@ import './main-page.css'
 function Main() {
     return (
         <>
-            <img alt="doc" className="main-pic" src="https://apexclinic.radiantthemes.com/dentist/wp-content/uploads/sites/4/2018/07/dentist-banner.jpg">
+            <img alt="doc" className="main-pic" src="../pics/dentist-banner.jpg">
 
             </img>
             <div style={{ width: '1200px', marginLeft: 'auto', marginRight: 'auto', textAlign: 'left',position: 'relative' }}>
