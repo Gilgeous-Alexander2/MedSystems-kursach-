467d316fbf463b437e876c5f15da41b022a7eeb7
Gilgeous-Alexander2 <117025505+Gilgeous-Alexander2@users.noreply.github.com>
2025-07-01 09:53:57 +0300

    Update header.js

--- env/my-app/src/components/header.js
+++ env/my-app/src/components/header.js
@@ -9,7 +9,6 @@ function Header() {
                     <ul style={{padding:0,margin:0}}>
                         <li className="menu-item"><Link to={"/"}>home</Link></li>
                         <li className="menu-item"><Link to={"/search"}>find a doctor</Link></li>
-                        <div className="menu-item"><Link to={"/profile"}>profile</Link></div>
                     </ul>
                 </nav>
                 <div style={{display: 'flex',flexGrow:1}}></div>
