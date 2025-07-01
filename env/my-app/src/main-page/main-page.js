6ab15c9d209cb4a79bc99de8f29c17d28aad072a
Gilgeous-Alexander2 <117025505+Gilgeous-Alexander2@users.noreply.github.com>
2025-07-01 10:25:22 +0300

    Update main-page.js

--- env/my-app/src/main-page/main-page.js
+++ env/my-app/src/main-page/main-page.js
@@ -3,9 +3,9 @@ import './main-page.css'
 function Main() {
     return (
         <>
-            <img alt="doc" className="main-pic" src="../pics/dentist-banner.jpg">
+            <div className="main-pic">
 
-            </img>
+            </div>
             <div style={{ width: '1200px', marginLeft: 'auto', marginRight: 'auto', textAlign: 'left',position: 'relative' }}>
                 <div style={{ position: 'absolute',top:'350px' }}>
                     <div className="main-pic-head">Complete Dental Solutions <br />
